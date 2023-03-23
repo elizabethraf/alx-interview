4c2002a3813ddb14c5b98742b47d45556427bd05
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-23 13:30:12 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Display Pascal triangle"""
+
+
 def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal triangle"""
     if n <= 0:
-        list = []
-    else:
-        list = [1]
-        for a in range(n):
-            print(list)
+        return []
+
+        list = [[1]]
+        for a in range(n - 1):
             newlist = []
-            newlist.append(list[0])
-            for a in range(len(list)-1):
-                newlist.append(list[a]+list[a+1])
-            newlist.append(list[-1])
-            list = newlist
+            prev = [0] + list[-1] + [0]
+            for b in range(len(list)-1):
+                newlist.append(prev[b] + prev[b+1])
+            list.append(newlist)
+        return list
