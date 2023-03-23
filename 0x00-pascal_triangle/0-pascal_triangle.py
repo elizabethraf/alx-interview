5f26e914027522b4f5da3c1092a58907a51a415e
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-23 09:57:38 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,9 +6,9 @@ def pascal_triangle(n):
         list = [1]
         for a in range(n):
             print(list)
-            newlist=[]
+            newlist = []
             newlist.append(list[0])
             for a in range(len(list)-1):
                 newlist.append(list[a]+list[a+1])
             newlist.append(list[-1])
-            list=newlist
+            list = newlist
