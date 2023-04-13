534b2e036398f9f56159b1f25190ffeea4c0c365
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-13 17:02:29 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
-"""Log parsing"""
+"""Display that reads stdin and computes metrics"""
 import sys
+from collections import defaultdict
 
-if __name__ == '__main__':
 
-    statusCodes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    count = 0
-    size = 0$
+def print_metrics(total_size, status_code_counts):
+    print("Total file size: {}".format(total_size))
+    for status_code in sorted(status_code_counts.keys()):
+        count = status_code_counts[status_code]
+        print("{}: {}".format(status_code, count))
 
-def printStatus(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+
+def process_line(line, total_size, status_code_counts):
+    parts = line.split()
+    if len(parts) != 9:
+        return total_size, status_code_counts
+    ip_address, _, _, timestamp, _, method, path, status_code, filesize = parts
+    try:
+        status_code = int(status_code)
+        file_size = int(parts[-1])
+    except ValueError:
+        return total_size, status_code_counts
+    total_size += file_size
+    status_code_counts[status_code] += 1
+    return total_size, status_code_counts
+
+
+total_size = 0
+status_code_counts = defaultdict(int)
+line_count = 0
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0
-            printStatus(statusCode, size)
-
-         stlist = line.split:
-             count += 1
-
-             try:
-                 size += int(stlist[-1])
-             except BaseException:
-                 pass
-
-             try:
-                 if stlist[-2] in statusCodes:
-                     statusCodes[stlist[-2]] += 1
-              except BaseException:
-                  pass
-              printStatus(statusCodes, size)
-
+        line_count += 1
+        total_size, status_code_counts = process_line
+        (line, total_size, status_code_counts)
+        if line_count % 10 == 0:
+            print_metrics(total_size, status_code_counts)
 except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
-
-
-
+    print_metrics(total_size, status_code_counts)
