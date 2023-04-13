afcbf5eead8d2e67990ec41dcdf5a552e44429e5
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-13 15:55:33 +0000

    0-stats.py debug

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
 """Display that reads stdin and computes metrics"""
 import sys
+from collections import defaultdict
 
 
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+def print_metrics(total_size, status_code_counts):
+    print("Total file size: {}".format(total_size))
+    for status_code in sorted(status_code_counts.keys()):
+        count = status_code_counts[status_code]
+        print("{}: {}".format(status_code, count))
 
 
+def process_line(line, total_size, status_code_counts):
+    parts = line.split()
+    print(parts)
+    if len(parts) != 9:
+        return total_size, status_code_counts
+    ip_address, _, _, timestamp, _, method, path, status_code, filesize = parts
+    try:
+        status_code = int(status_code)
+        file_size = int(parts[-1])
+    except ValueError:
+        return total_size, status_code_counts
+    total_size += file_size
+    print(status_code_counts)
+    status_code_counts[status_code] += 1
+    return total_size, status_code_counts
+
+
+total_size = 0
+status_code_counts = defaultdict(int)
 line_count = 0
-file_sizes = {}
-total_file_size = 0
-status_code_counts = {}
 
 try:
     for line in sys.stdin:
         line_count += 1
-
-        try:
-            ip_address, _, date, request, status_code_str, file_size_str, _ = line.split()
-            status_code = int(status_code_str)
-            file_size = int(file_size_str)
-
-            if status_code in status_codes:
-                if status_code not in status_code_counts:
-                    status_code_counts[status_code] = 0
-                status_code_counts[status_code] += 1
-
-            if file_size > 0:
-                total_file_size += file_size
-                if request not in file_sizes:
-                    file_sizes[request] = 0
-                file_sizes[request] += file_size
-
-        except ValueError:
-            continue
-
+        total_size, status_code_counts = process_line(line, total_size, status_code_counts)
         if line_count % 10 == 0:
-            print("Total file size: File size: {}".format(total_file_size))
-            for status_code in sorted(status_code_counts.keys()):
-                print("{}: {}".format(status_code, status_code_counts[status_code]))
-            line_count = 0
-
-    print("Total file size: File size: {}".format(total_file_size))
-    for status_code in sorted(status_code_counts.keys()):
-        print("{}: {}".format(status_code, status_code_counts[status_code]))
-
-except BrokenPipeError:
-    pass
+            print_metrics(total_size, status_code_counts)
+except KeyboardInterrupt:
+    print_metrics(total_size, status_code_counts)
