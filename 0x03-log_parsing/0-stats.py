e638e0563f9210799412b9ccea4c2512bc05fcbb
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-16 18:53:12 +0000

    0-stats_fix.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,39 +3,43 @@
 import sys
 from collections import defaultdict
 
-
-def print_metrics(total_size, status_code_counts):
-    print("Total file size: {}".format(total_size))
-    for status_code in sorted(status_code_counts.keys()):
-        count = status_code_counts[status_code]
-        print("{}: {}".format(status_code, count))
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+sum = 0
 
 
-def process_line(line, total_size, status_code_counts):
-    parts = line.split()
-    if len(parts) != 9:
-        return total_size, status_code_counts
-    ip_address, _, _, timestamp, _, method, path, status_code, filesize = parts
+def prev_stats():
+    """Function that print stats"""
+    global sum
+
+    print('File size: {}'.format(sum))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
+
+
+if __name__ == "__main__":
+    count = 0
     try:
-        status_code = int(status_code)
-        file_size = int(parts[-1])
-    except ValueError:
-        return total_size, status_code_counts
-    total_size += file_size
-    status_code_counts[status_code] += 1
-    return total_size, status_code_counts
-
-
-total_size = 0
-status_code_counts = defaultdict(int)
-line_count = 0
-
-try:
-    for line in sys.stdin:
-        line_count += 1
-        total_size, status_code_counts = process_line
-        (line, total_size, status_code_counts)
-        if line_count % 10 == 0:
-            print_metrics(total_size, status_code_counts)
-except KeyboardInterrupt:
-    print_metrics(total_size, status_code_counts)
+        """ Iter the standar input """
+        for data in sys.stdin:
+            try:
+                fact = data.split(' ')
+                """If there is a status code"""
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """If there is a lenght"""
+                sum += int(fact[-1])
+            except:
+                pass
+            """Printing control"""
+            count += 1
+            if count == 10:
+                prev_stats()
+                count = 0
+    except KeyboardInterrupt:
+        prev_stats()
+        raise
+    else:
+        prev_stats()
