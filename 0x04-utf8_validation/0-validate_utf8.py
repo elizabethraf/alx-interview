82196178f05849dd1382666cd85310c706e10e2b
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-20 16:57:42 +0000

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Method that determines data set represents a valid UTF-8 encodin"""
+
+
+def validUTF8(data):
+    """Initialise UTF-8 encoding"""
+    pre_bytes = 0
+
+    for byte in data:
+
+        if pre_bytes == 0:
+            if (byte >> 5) == 0b110:
+                pre_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                pre_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                pre_bytes = 3
+            elif (byte >> 7) != 0:
+                return False
+
+        elif (byte >> 6) != 0b10:
+            return False
+
+        else:
+            pre_bytes -= 1
+    return pre_bytes == 0
