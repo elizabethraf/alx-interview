f4db5ac1cdac3470e1b0487063298f5d9546feac
elizabethraf <elizabethra@matamaneholdings.co.za>
2023-05-10 01:58:52 -0700

    0-rotate_2d_matrix.py

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Display 2D matrix, rotate it 90 degrees clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    """Display 2D matrix"""
+    n = len(matrix)
+
+    for b in range(n):
+        for j in range(b, n):
+            matrix[b][j], matrix[j][b] = matrix[j][b], matrix[b][j]
+
+    for b in range(n):
+        for j in range(n // 2):
+            matrix[b][j], matrix[b][n - j - 1] = matrix[b] \
+               [n - j - 1], matrix[b][j]
