a8af3b0d8d14e510df0bc439cbe4960ae7381b63
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-06-13 15:58:16 +0000

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,33 +4,19 @@
 
 def island_perimeter(grid):
     """Display Island Perimeter"""
-    if not grid or not grid[0]:
+    if not grid:
         return 0
 
-    rows = len(grid)
-    cols = len(grid[0])
     perimeter = 0
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
-    for i in range(rows):
-        if grid[i][0] == 1:
-            perimeter -= 1
-        if grid[i][cols - 1] == 1:
-            perimeter -= 1
-
-    for j in range(cols):
-        if grid[0][j] == 1:
-            perimeter -= 1
-        if grid[rows - 1][j] == 1:
-            perimeter -= 1
-
+    for rows in range(len(grid)):
+        for cols in range(len(grid[rows])):
+            if grid[rows][cols] == 1:
+                if rows == 0 or grid[rows - 1][cols] == 0:
+                    perimeter += 1
+                if cols == 0 or grid[rows][cols - 1] == 0:
+                    perimeter += 1
+                if rows == len(grid) - 1 or grid[rows + 1][cols] == 0:
+                    perimeter += 1
+                if cols == len(grid[rows]) - 1 or grid[rows][cols + 1] == 0:
+                    perimeter += 1
     return perimeter
